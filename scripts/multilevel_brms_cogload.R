ea17f98f6e263c82d50ac76d1f62eb5f1fdfd00e
Miguel Rios <mriosb08@gmail.com>
2023-09-28 11:53:34 +0200

    update names

--- scripts/multilevel_brms_cogload.R
+++ scripts/multilevel_brms_cogload.R
@@ -31,21 +31,23 @@ library(ggh4x)
 set.seed(1234) #reproducibility
 bayes_seed <- 1234
 #set_cmdstan_path(path = '/home/mrios/.cmdstan/cmdstan-2.32.2')
-eyetracking <- read.csv('/home/mrios/workspace/test_R/ANOVA_table3.xlsx - Sheet1.csv', header = TRUE, sep = ",")
+eyetracking <- read.csv('/home/mrios/workspace/test_R/quality_cogload_table.csv', header = TRUE, sep = ",")
 eyetracking
 eyetracking %>% sample_n_by(condition, text, size = 1)
-
+#delete NA
+eyetracking <- na.omit(eyetracking)
+colnames(eyetracking)
 #summary stats
 stats <- eyetracking %>%
   group_by(condition, text) %>%
-  get_summary_stats(quality_score, type = "mean_sd")
+  get_summary_stats(MFD_ST, type = "mean_sd")
 stats
-write.csv(stats,'/home/mrios/workspace/test_R/quality/summary_stats.csv')
+write.csv(stats,'/home/mrios/workspace/test_R/cog_load/summary_stats.csv')
 
 ##
 #condition model
 ##
-fit0 <- brm(formula = quality_score ~ 1 + condition, #(1 + condition | participant)
+fit0 <- brm(formula = MFD_ST ~ 1 + condition, #(1 + condition | participant)
             data = eyetracking,
             warmup = 1000, iter = 5000, chains = 4, cores = 6,
             seed = bayes_seed,
@@ -53,14 +55,14 @@ fit0 <- brm(formula = quality_score ~ 1 + condition, #(1 + condition | participa
 )
 fit0
 text_summ <-summary(fit0)
-sink("/home/mrios/workspace/test_R/quality/multilevel_brms0_summary.txt")
+sink("/home/mrios/workspace/test_R/cog_load/multilevel_brms0_summary.txt")
 text_summ
 #tidy(fit1)
 sink()
 #sjPlot::tab_model(fit1)
 fit0
 p_summary <- posterior_summary(fit0)
-write.csv(p_summary, '/home/mrios/workspace/test_R/quality/multilevel_brms0_psummary.csv')
+write.csv(p_summary, '/home/mrios/workspace/test_R/cog_load/multilevel_brms0_psummary.csv')
 
 plot(fit0)
 pp_check(fit0)
@@ -68,18 +70,19 @@ pp_check(fit0)
 fixef(fit0)
 #coef(fit0)  #coef(fit1)$condition TODO
 #coef(fit1)$text[conditions]
-
 conditional_effects(fit0)
+
+
 
 loo0 <- loo(fit0) 
 loo0
 
 tidy(fit0)
 ###
-#quality_score ~ 1 + condition + text 
+#MFD_ST ~ 1 + condition + text 
 ###
 
-fit0b <- brm(formula = quality_score ~ 1 + condition + text, #(1 + condition | participant)
+fit0b <- brm(formula = MFD_ST ~ 1 + condition + text, #(1 + condition | participant)
             data = eyetracking,
             warmup = 1000, iter = 5000, chains = 4, cores = 6,
             seed = bayes_seed,
@@ -87,14 +90,14 @@ fit0b <- brm(formula = quality_score ~ 1 + condition + text, #(1 + condition | p
 )
 fit0b
 text_summ <-summary(fit0b)
-sink("/home/mrios/workspace/test_R/quality/multilevel_brms0b_summary.txt")
+sink("/home/mrios/workspace/test_R/cog_load/multilevel_brms0b_summary.txt")
 text_summ
 #tidy(fit1)
 sink()
 #sjPlot::tab_model(fit1)
 fit0b
 p_summary <- posterior_summary(fit0b)
-write.csv(p_summary, '/home/mrios/workspace/test_R/quality/multilevel_brms0b_psummary.csv')
+write.csv(p_summary, '/home/mrios/workspace/test_R/cog_load/multilevel_brms0b_psummary.csv')
 
 plot(fit0b)
 pp_check(fit0b)
@@ -105,16 +108,18 @@ fixef(fit0b)
 
 conditional_effects(fit0b)
 
+
 loo0b <- loo(fit0b) 
 loo0b
 
 tidy(fit0b)
 
+loo_compare(loo0, loo0b)
 ##
-#multilevel quality_score ~ 1 + condition + text + (1 + condition | text)
+#multilevel MFD_ST ~ 1 + condition + text + (1 + condition | text)
 ##
 
-fit1 <- brm(formula = quality_score ~ 1 + condition + text + (1 + condition | text), #(1 + condition | participant)
+fit1 <- brm(formula = MFD_ST ~ 1 + condition + text + (1 + condition | text), #(1 + condition | participant)
             data = eyetracking,
             warmup = 1000, iter = 10000, chains = 4, cores = 6,
             control=list(adapt_delta=0.9), seed = bayes_seed,
@@ -122,17 +127,17 @@ fit1 <- brm(formula = quality_score ~ 1 + condition + text + (1 + condition | te
             )
 fit1
 text_summ <-summary(fit1)
-sink("/home/mrios/workspace/test_R/quality/multilevel_brms1_summary.txt")
+sink("/home/mrios/workspace/test_R/cog_load/multilevel_brms1_summary.txt")
 text_summ
 #tidy(fit1)
 sink()
 #sjPlot::tab_model(fit1)
 fit1
 p_summary <- posterior_summary(fit1)
-write.csv(p_summary, '/home/mrios/workspace/test_R/quality/multilevel_brms1_psummary.csv')
+write.csv(p_summary, '/home/mrios/workspace/test_R/cog_load/multilevel_brms1_psummary.csv')
 randeff <- ranef(fit1)
 randeff
-write.csv(randeff, '/home/mrios/workspace/test_R/quality/multilevel_brms1_randeff.csv')
+write.csv(randeff, '/home/mrios/workspace/test_R/cog_load/multilevel_brms1_randeff.csv')
 
 plot(fit1)
 pp_check(fit1)
@@ -142,19 +147,24 @@ coef(fit1)  #coef(fit1)$condition TODO
 #coef(fit1)$text[conditions]
 extract_random_effects(fit1)
 
+
 conditional_effects(fit1)
+
+
 
 loo1 <- loo(fit1) 
 loo1
 
+loo_compare(loo0, loo1)
+
 (model_fit <- eyetracking %>%
     add_predicted_draws(fit1) %>%  # adding the posterior distribution
-    ggplot(aes(x = text, y = quality_score)) +  
+    ggplot(aes(x = text, y = MFD_ST)) +  
     stat_lineribbon(aes(y = .prediction), .width = c(.95, .80, .50),  # regression line and CI
                     alpha = 0.5, colour = "black") +
     geom_point(data = eyetracking, colour = "darkseagreen4", size = 3) +   # raw data
     scale_fill_brewer(palette = "Greys") +
-    ylab("quality_score\n") +  # latin name for red knot
+    ylab("MFD_ST\n") +  # latin name for red knot
     xlab("\ntext") +
     theme_bw() +
     theme(legend.title = element_blank(),
@@ -162,12 +172,12 @@ loo1
 
 (model_fit <- eyetracking %>%
     add_predicted_draws(fit1) %>%  # adding the posterior distribution
-    ggplot(aes(x = condition, y = quality_score)) +  
+    ggplot(aes(x = condition, y = MFD_ST)) +  
     stat_lineribbon(aes(y = .prediction), .width = c(.95, .80, .50),  # regression line and CI
                     alpha = 0.5, colour = "black") +
     geom_point(data = eyetracking, colour = "darkseagreen4", size = 3) +   # raw data
     scale_fill_brewer(palette = "Greys") +
-    ylab("quality_score\n") +  # latin name for red knot
+    ylab("MFD_ST\n") +  # latin name for red knot
     xlab("\ncondition") +
     theme_bw() +
     theme(legend.title = element_blank(),
@@ -176,11 +186,13 @@ loo1
 
 tidy(fit1)
 #text level random effects
+#https://www.andrewheiss.com/blog/2021/12/01/multilevel-models-panel-data-guide/#intercepts-and-slopes-for-each-country
 condition_text_offsets <- ranef(fit1)$text %>%
   as_tibble(rownames = "text") %>% 
   filter(text %in% eyetracking$text) %>% 
   select(text, starts_with("Estimate"))
 condition_text_offsets
+write.csv(condition_text_offsets, '/home/mrios/workspace/test_R/cog_load/multilevel_brms1_condoffsets.csv')
 #fixed effect + random offset for text-specific intercepts and slopes.
 coef(fit1)$text %>%
   as_tibble(rownames = "text") %>% 
@@ -191,9 +203,9 @@ coef(fit1)$text %>%
 
 ####
 #model participant
-#quality_score ~ 1 + condition + text + (1 + condition | participant)
+#MFD_ST ~ 1 + condition + text + (1 + condition | participant)
 ####
-fit2 <- brm(formula = quality_score ~ 1 + condition + text + (1 + condition | participant), 
+fit2 <- brm(formula = MFD_ST ~ 1 + condition + text + (1 + condition | participant), 
             data = eyetracking,
             warmup = 1000, iter = 10000, chains = 4, cores = 6,
             control=list(adapt_delta=0.9), seed = bayes_seed,
@@ -201,7 +213,7 @@ fit2 <- brm(formula = quality_score ~ 1 + condition + text + (1 + condition | pa
             )
 fit2
 text_summ <-summary(fit2)
-sink("/home/mrios/workspace/test_R/quality/multilevel_brms2_summary.txt")
+sink("/home/mrios/workspace/test_R/cog_load/multilevel_brms2_summary.txt")
 text_summ
 sink()
 #print(fit2)
@@ -209,32 +221,36 @@ sink()
 fit2
 p_summary <- posterior_summary(fit2)
 p_summary
-write.csv(p_summary, '/home/mrios/workspace/test_R/quality/multilevel_brms2_psummary.csv')
+write.csv(p_summary, '/home/mrios/workspace/test_R/cog_load/multilevel_brms2_psummary.csv')
 #write.csv(text_summ, '/home/mrios/workspace/test_R/quality/multilevel_brms1_summary.txt')
 randeff <- ranef(fit2)
 randeff
-write.csv(randeff, '/home/mrios/workspace/test_R/quality/multilevel_brms2_randeff.csv')
+write.csv(randeff, '/home/mrios/workspace/test_R/cog_load/multilevel_brms2_randeff.csv')
 plot(fit2)
 pp_check(fit2)
 
 fixef(fit2)
 coef(fit2) 
 extract_random_effects(fit2)
+
+
 conditional_effects(fit2)
+
+
 loo2 <- loo(fit2)
 
-
+loo_compare(loo1, loo2)
 loo_compare(loo0, loo0b, loo1, loo2)
 
 
 (model_fit <- eyetracking %>%
     add_predicted_draws(fit2) %>%  # adding the posterior distribution
-    ggplot(aes(x = text, y = quality_score)) +  
+    ggplot(aes(x = text, y = cog_load)) +  
     stat_lineribbon(aes(y = .prediction), .width = c(.95, .80, .50),  # regression line and CI
                     alpha = 0.5, colour = "black") +
     geom_point(data = eyetracking, colour = "darkseagreen4", size = 3) +   # raw data
     scale_fill_brewer(palette = "Greys") +
-    ylab("quality_score\n") +  # latin name for red knot
+    ylab("cog_load\n") +  # latin name for red knot
     xlab("\ntext") +
     theme_bw() +
     theme(legend.title = element_blank(),
@@ -242,12 +258,12 @@ loo_compare(loo0, loo0b, loo1, loo2)
 
 (model_fit <- eyetracking %>%
     add_predicted_draws(fit2) %>%  # adding the posterior distribution
-    ggplot(aes(x = condition, y = quality_score)) +  
+    ggplot(aes(x = condition, y = MFD_ST)) +  
     stat_lineribbon(aes(y = .prediction), .width = c(.95, .80, .50),  # regression line and CI
                     alpha = 0.5, colour = "black") +
     geom_point(data = eyetracking, colour = "darkseagreen4", size = 3) +   # raw data
     scale_fill_brewer(palette = "Greys") +
-    ylab("quality_score\n") +  # latin name for red knot
+    ylab("MFD_ST\n") +  # latin name for red knot
     xlab("\ncondition") +
     theme_bw() +
     theme(legend.title = element_blank(),
@@ -259,6 +275,7 @@ condition_participant_offsets <- ranef(fit2)$participant %>%
   filter(participant %in% eyetracking$participant) %>% 
   select(participant, starts_with("Estimate"))
 condition_participant_offsets
+write.csv(condition_participant_offsets, '/home/mrios/workspace/test_R/cog_load/multilevel_brms2_condoffsets.csv')
 #fixed effect + random offset for text-specific intercepts and slopes.
 coef(fit2)$participant %>%
   as_tibble(rownames = "participant") %>% 
--- scripts/multilevel_brms_quality.R
+++ scripts/multilevel_brms_quality.R
@@ -1,9 +1,11 @@
 library(brms)
+library(cmdstanr)
 library(tidyverse)
 library(sjPlot)
 #install.packages("tidybayes")
 library(tidybayes)
 library(sjstats)
+library(gapminder)
 #install.packages("sjstats")
 #install.packages('assertthat')
 #install.packages('remotes')
@@ -19,11 +21,17 @@ library(assertthat)
 library(report)
 library(ggpubr)
 library(rstatix)
-#library(mixedup)
+library(mixedup)
+library(broom.mixed)
+library(ggplot2)
+library(dplyr)
+library(ggh4x)
+
 
 set.seed(1234) #reproducibility
 bayes_seed <- 1234
-eyetracking <- read.csv('/home/mrios/workspace/test_R/ANOVA_table3.xlsx - Sheet1.csv', header = TRUE, sep = ",")
+#set_cmdstan_path(path = '/home/mrios/.cmdstan/cmdstan-2.32.2')
+eyetracking <- read.csv('/home/mrios/workspace/test_R/quality_table.csv', header = TRUE, sep = ",")
 eyetracking
 eyetracking %>% sample_n_by(condition, text, size = 1)
 
@@ -34,20 +42,20 @@ stats <- eyetracking %>%
 stats
 write.csv(stats,'/home/mrios/workspace/test_R/quality/summary_stats.csv')
 
-####
-#model condition
-####
-
+##
+#condition model
+##
 fit0 <- brm(formula = quality_score ~ 1 + condition, #(1 + condition | participant)
             data = eyetracking,
-            warmup = 1000, iter = 5000, chains = 4, cores = 6,
-            seed = bayes_seed
+            warmup = 1000, iter = 10000, chains = 4, cores = 6,
+            seed = bayes_seed,
+            backend = "cmdstanr"
 )
 fit0
 text_summ <-summary(fit0)
 sink("/home/mrios/workspace/test_R/quality/multilevel_brms0_summary.txt")
 text_summ
-#tidy(fit0)
+#tidy(fit1)
 sink()
 #sjPlot::tab_model(fit1)
 fit0
@@ -59,35 +67,61 @@ pp_check(fit0)
 
 fixef(fit0)
 #coef(fit0)  #coef(fit1)$condition TODO
-png(file="/home/mrios/workspace/test_R/quality/fit0.png",
-    width=600, height=350)
+#coef(fit1)$text[conditions]
 conditional_effects(fit0)
-dev.off()
+
 
 
 loo0 <- loo(fit0) 
 loo0
 
-#(model_fit <- eyetracking %>%
-#    add_predicted_draws(fit0) %>%  # adding the posterior distribution
-#    ggplot(aes(x = text, y = quality_score)) +  
-#    stat_lineribbon(aes(y = .prediction), .width = c(.95, .80, .50),  # regression line and CI
-#                    alpha = 0.5, colour = "black") +
- #   geom_point(data = eyetracking, colour = "darkseagreen4", size = 3) +   # raw data
-#    scale_fill_brewer(palette = "Greys") +
- #   ylab("quality_score\n") +  # latin name for red knot
-  #  xlab("\ntext") +
-  #  theme_bw() +
-   # theme(legend.title = element_blank(),
-  #        legend.position = c(0.15, 0.85)))
+tidy(fit0)
+###
+#quality_score ~ 1 + condition + text 
+###
+
+fit0b <- brm(formula = quality_score ~ 1 + condition + text, #(1 + condition | participant)
+            data = eyetracking,
+            warmup = 1000, iter = 10000, chains = 4, cores = 6,
+            seed = bayes_seed,
+            backend = "cmdstanr"
+)
+fit0b
+text_summ <-summary(fit0b)
+sink("/home/mrios/workspace/test_R/quality/multilevel_brms0b_summary.txt")
+text_summ
+#tidy(fit1)
+sink()
+#sjPlot::tab_model(fit1)
+fit0b
+p_summary <- posterior_summary(fit0b)
+write.csv(p_summary, '/home/mrios/workspace/test_R/quality/multilevel_brms0b_psummary.csv')
+
+plot(fit0b)
+pp_check(fit0b)
+
+fixef(fit0b)
+#coef(fit0)  #coef(fit1)$condition TODO
+#coef(fit1)$text[conditions]
+
+conditional_effects(fit0b)
 
 
-####
+loo0b <- loo(fit0b) 
+loo0b
+
+tidy(fit0b)
+
+loo_compare(loo0, loo0b)
+##
+#multilevel quality_score ~ 1 + condition + text + (1 + condition | text)
+##
 
 fit1 <- brm(formula = quality_score ~ 1 + condition + text + (1 + condition | text), #(1 + condition | participant)
             data = eyetracking,
-            warmup = 1000, iter = 5000, chains = 4, cores = 6,
-            control=list(adapt_delta=0.99), seed = bayes_seed
+            warmup = 1000, iter = 10000, chains = 4, cores = 6,
+            control=list(adapt_delta=0.9), seed = bayes_seed,
+            backend = "cmdstanr"
             )
 fit1
 text_summ <-summary(fit1)
@@ -108,16 +142,18 @@ pp_check(fit1)
 
 fixef(fit1)
 coef(fit1)  #coef(fit1)$condition TODO
+#coef(fit1)$text[conditions]
 extract_random_effects(fit1)
 
-png(file="/home/mrios/workspace/test_R/quality/fit1.png",
-    width=600, height=350)
+
 conditional_effects(fit1)
-dev.off()
+
 
 
 loo1 <- loo(fit1) 
 loo1
+
+loo_compare(loo0, loo1)
 
 (model_fit <- eyetracking %>%
     add_predicted_draws(fit1) %>%  # adding the posterior distribution
@@ -145,14 +181,34 @@ loo1
     theme(legend.title = element_blank(),
           legend.position = c(0.15, 0.85)))
 
+
+tidy(fit1)
+#text level random effects
+#https://www.andrewheiss.com/blog/2021/12/01/multilevel-models-panel-data-guide/#intercepts-and-slopes-for-each-country
+condition_text_offsets <- ranef(fit1)$text %>%
+  as_tibble(rownames = "text") %>% 
+  filter(text %in% eyetracking$text) %>% 
+  select(text, starts_with("Estimate"))
+condition_text_offsets
+write.csv(condition_text_offsets, '/home/mrios/workspace/test_R/quality/multilevel_brms1_condoffsets.csv')
+#fixed effect + random offset for text-specific intercepts and slopes.
+coef(fit1)$text %>%
+  as_tibble(rownames = "text") %>% 
+  filter(text %in% eyetracking$text) %>% 
+  select(text, starts_with("Estimate"))
+
+
+
 ####
 #model participant
+#quality_score ~ 1 + condition + text + (1 + condition | participant)
 ####
 fit2 <- brm(formula = quality_score ~ 1 + condition + text + (1 + condition | participant), 
             data = eyetracking,
-            warmup = 1000, iter = 5000, chains = 4, cores = 6,
-            control=list(adapt_delta=0.99), seed = bayes_seed
-)
+            warmup = 1000, iter = 10000, chains = 4, cores = 6,
+            control=list(adapt_delta=0.9), seed = bayes_seed,
+            backend = "cmdstanr"
+            )
 fit2
 text_summ <-summary(fit2)
 sink("/home/mrios/workspace/test_R/quality/multilevel_brms2_summary.txt")
@@ -174,11 +230,17 @@ pp_check(fit2)
 fixef(fit2)
 coef(fit2) 
 extract_random_effects(fit2)
+
+
 conditional_effects(fit2)
+
+
 loo2 <- loo(fit2)
 
-
-loo_compare(loo0, loo1, loo2)
+loo_cpm <- loo_compare(loo1, loo2)
+loo_cpm
+write.csv(loo_cpm, '/home/mrios/workspace/test_R/quality/multilevel_brms_loo_cpm1-2.csv')
+loo_compare(loo0, loo0b, loo1, loo2)
 
 
 (model_fit <- eyetracking %>%
@@ -207,15 +269,18 @@ loo_compare(loo0, loo1, loo2)
     theme(legend.title = element_blank(),
           legend.position = c(0.15, 0.85)))
 
-#####
-#condtion * text
-#####
+#participant level random effects
+condition_participant_offsets <- ranef(fit2)$participant %>%
+  as_tibble(rownames = "participant") %>% 
+  filter(participant %in% eyetracking$participant) %>% 
+  select(participant, starts_with("Estimate"))
+condition_participant_offsets
+write.csv(condition_participant_offsets, '/home/mrios/workspace/test_R/quality/multilevel_brms2_condoffsets.csv')
+#fixed effect + random offset for text-specific intercepts and slopes.
+coef(fit2)$participant %>%
+  as_tibble(rownames = "participant") %>% 
+  filter(participant %in% eyetracking$participant) %>% 
+  select(participant, starts_with("Estimate"))
 
-#fit3 <- brm(formula = quality_score ~ 1 + condition + text + condition*text, 
-#            data = eyetracking,
-#            warmup = 1000, iter = 10000, chains = 4, cores = 10,
-#            control=list(adapt_delta = 0.999), seed = bayes_seed
-#            )
-#fit3
-#conditional_effects(fit3)
+
 
